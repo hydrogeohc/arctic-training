0d77ac934fa64aeafbdd2faac975992949a67dcf
Ying-Jung Deweese <ydeweese@uw.edu>
2019-01-17 16:03:45 -0800

    change_400pm

---
+++ fahr_to_celsius.R
@@ -0,0 +1,18 @@
+
+fahr_to_celsius <- function(fahr) {
+  celsius <- (fahr-32)*5/9
+  return(celsius)
+}
+
+
+celsius_to_fahr<-function(celsius){
+  fahr<-(celsius*9/5)+32
+  return(fahr)
+}
+
+celsiusok=fahr_to_celsius(airtemps)
+fahrok=celsius_to_fahr(celsiusok)
+airtemps==fahrok
+
+temp_dif_units=data.frame(celsiusok,fahrok)
+write.csv(temp_dif_units,file="temp_dif_u.csv")
